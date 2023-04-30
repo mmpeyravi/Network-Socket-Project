c225494c0211cf822271ea4be27617a757c87489
mmpeyravi <50866430+mmpeyravi@users.noreply.github.com>
2023-04-30 09:36:09 +0330

    Add files via upload

---
+++ ip_detect.py
@@ -0,0 +1,11 @@
+
+import socket    
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr)
+# Python program to demonstrate
+# while loop with True
+socket.close(0);
+while True:
+	pass
